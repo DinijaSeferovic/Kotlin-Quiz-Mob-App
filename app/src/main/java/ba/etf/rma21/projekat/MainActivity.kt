837a35c3937de61fe02f651e7c5a3f6077690a35
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2021-04-09 16:49:28 +0200

    Prvi zadatak (svi testovi)

--- app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
@@ -11,11 +11,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma21.projekat.data.models.Kviz
 import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.getAll
-import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.getDone
-import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.getFuture
-import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.getMyKvizes
-import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.getNotTaken
-import ba.etf.rma21.projekat.data.repositories.KvizRepository.Companion.sortirajKviz
 import ba.etf.rma21.projekat.view.KvizListAdapter
 import ba.etf.rma21.projekat.viewmodel.KvizListViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -30,11 +25,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var spinnerAdapter: ArrayAdapter<Kviz>
     private lateinit var upisButton : FloatingActionButton
     var categories = arrayOf("Svi moji kvizovi", "Svi kvizovi", "Urađeni kvizovi", "Budući kvizovi", "Prošli kvizovi")
+    var data : List<Kviz> = emptyList()
 
     private fun initializeViews() {
 
         mySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, categories)
-        kvizAdapter = KvizListAdapter(listOf())
+        kvizAdapter = KvizListAdapter(data)
         kvizovi.adapter = kvizAdapter
         //spinner selection events
         mySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -51,35 +47,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun getSelectedCategoryData(categoryID: Int) {
         //list to hold selected
-        var data : List<Kviz> = listOf()
-        spinnerAdapter = if (categoryID == 0) {
-            ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, sortirajKviz(getMyKvizes()))
 
-        } else {
+        spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, data)
+
             //filter by id
             for (Kviz in getAll()) {
-                if (categoryID == 1) {
-                    data= sortirajKviz(getAll())
-                    spinnerAdapter.notifyDataSetChanged()
+                if (categoryID == 0) {
+                    data= kvizListViewModel.getMojiKvizovi()
+                    kvizAdapter.updateKvizovi(data)
+                } else if (categoryID == 1) {
+                    data= kvizListViewModel.getKvizovi()
+                    kvizAdapter.updateKvizovi(data)
                 } else if (categoryID == 2) {
-                    data= sortirajKviz(getDone())
-                    spinnerAdapter.notifyDataSetChanged()
+                    data= kvizListViewModel.getGotoviKvizovi()
+                    kvizAdapter.updateKvizovi(data)
                 } else if (categoryID == 3) {
-                    data= sortirajKviz(getFuture())
-                    spinnerAdapter.notifyDataSetChanged()
+                    data= kvizListViewModel.getBuduciKvizovi()
+                    kvizAdapter.updateKvizovi(data)
                 } else if (categoryID == 4) {
-                    data= sortirajKviz(getNotTaken())
-                    spinnerAdapter.notifyDataSetChanged()
+                    data= kvizListViewModel.getNeodrzaniKvizovi()
+                    kvizAdapter.updateKvizovi(data)
 
                 }
-            }
+
             //instatiate adapter a
             ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, data)
         }
-
     }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
